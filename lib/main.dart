aa84d3224351cc0365f9007ac05b6170b1e99bd8
muhammadmubashir24 <muhammad.mubashir2498@gmail.com>
2024-03-14 16:25:59 +0500

    Flutter Updated Class Task Perform and Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8.0),
+            child: Column(
+              children: [
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.teal,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+                FilledButton.tonal(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
